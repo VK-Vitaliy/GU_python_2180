9cf376ddeae423fa878159476302beb39d32e084
VK-Vitaliy <kulvitall.vk@gmail.com>
2022-03-28 16:27:03 +0300

    dz_5 v2

--- Kuleshov_Vitaliy_dz_5/task_5_2.py
+++ Kuleshov_Vitaliy_dz_5/task_5_2.py
@@ -1,3 +1,4 @@
+# Решить задачу генерации нечётных чисел от 1 до n (включительно), не используя ключевое слово yield
 odd_to_n = 15
 odd_nums = (i for i in range(1, odd_to_n + 1, 2))
 print(next(odd_nums))
--- Kuleshov_Vitaliy_dz_5/task_5_3.py
+++ Kuleshov_Vitaliy_dz_5/task_5_3.py
@@ -1,3 +1,7 @@
+# Есть два списка. Необходимо реализовать генератор, возвращающий кортежи вида (<tutor>, <klass>)
+# Количество генерируемых кортежей не должно быть больше длины списка tutors.
+# Если в списке klasses меньше элементов, чем в списке tutors,
+# необходимо вывести последние кортежи в виде: (<tutor>, None)
 tutors = [
     'Иван', 'Анастасия', 'Петр', 'Сергей',
     'Дмитрий', 'Борис', 'Елена', 'Дмитрий', 'Станислав'
@@ -6,10 +10,7 @@ klasses = [
     '9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А'
 ]
 
-if len(tutors) >= len(klasses):
-    klasses.append(None)
-
-person_new = ((tutors[i], klasses[i]) for i in range(len(tutors)))
+person_new = ((tutors[i], klasses[i]) if len(klasses) > i else (tutors[i], None) for i in range(len(tutors)))
 print(type(person_new))  # доказать что создали генератор
 
 for i in range(len(tutors)):
