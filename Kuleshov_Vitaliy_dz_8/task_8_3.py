4590875fac6b7b1cc33b3046929d1ee756b00e6a
VK-Vitaliy <kulvitall.vk@gmail.com>
2022-04-04 13:53:42 +0300

    dz_8 task_8_4

--- Kuleshov_Vitaliy_dz_8/task_8_3.py
+++ Kuleshov_Vitaliy_dz_8/task_8_3.py
@@ -1,19 +1,28 @@
-# Написать декоратор для логирования типов позиционных аргументов функции, например
+# Написать декоратор для логирования типов позиционных аргументов функции.
+# Примечание: если аргументов несколько - выводить данные о каждом через запятую;
+# можете ли вы вывести тип значения функции? Сможете ли решить задачу для именованных аргументов?
+# Сможете ли вы замаскировать работу декоратора? Сможете ли вывести имя функции
+from functools import wraps
+
+
 def type_logger(func):
-    def wrapper(*args):
-        result_func = func(*args)
+    @wraps(func)
+    def wrapper(*args, **kwargs):
         result_dict = {}
+        # работа с позиционными аргументами
         for i in args:
-            result_dict[i] = type(result_func)
-        print(func.__name__ + str(result_dict))
+            result_dict[i] = type(i)
+        # работа с именованными аргументами
+        for v in kwargs.values():
+            result_dict[v] = type(v)
+        return func.__name__ + str(result_dict)
 
     return wrapper
 
 
 @type_logger
-def calc_cube(*args):
-    for i in args:
-        return i ** 3
+def calc_cube(x):
+    return x ** 3
 
 
-calc_cube(5)
+print(calc_cube(5, 7, x=6))
